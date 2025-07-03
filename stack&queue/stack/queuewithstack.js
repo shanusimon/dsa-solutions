cf38721b904454125f9b68781082886bc2bdb3b0
Shanu Simon <shanusimon501@gmail.com>
2025-07-03 16:16:35 +0530

    added some comments

--- stack&queue/stack/queuewithstack.js
+++ stack&queue/stack/queuewithstack.js
@@ -15,6 +15,8 @@ class QueueWithStack {
       console.log("The Stack Is Empty");
       return;
     }
+    //stack1 is to add elements 
+    //in this first we need to add values from stack 1 using pop and push it into stack2 to get reversed 
     if (this.stack2.length === 0) {
       while (this.stack1.length > 0) {
         this.stack2.push(this.stack1.pop());
@@ -25,7 +27,7 @@ class QueueWithStack {
   peek() {
     if (this.isEmpty()) return null;
 
-    if (this.stack1.length === 0) {
+    if (this.stack2.length === 0) {
       while (this.stack1.length > 0) {
         this.stack2.push(this.stack1.pop());
       }
